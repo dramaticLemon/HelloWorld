ef0f74888024b18e7589dde1822224a36f583e63
dramaticLemon <chebandima27@gmail.com>
2025-05-11 16:13:32 +0300

    Converting Between Numbers ans String

---
+++ simple_project/src/ValueOfDemo.java
@@ -0,0 +1,20 @@
+public class ValueOfDemo {
+    public static void main(String[] args) {
+
+        if (args.length == 2) {
+            float a = (Float.valueOf(args[0])).floatValue();
+            float b = (Float.valueOf(args[1])).floatValue();
+            /*
+            float a = Float.parseFloat(args[0])
+             */
+            System.out.println("a + b = " + (a + b));
+            System.out.println("a - b = " + (a - b));
+            System.out.println("a * b = " + (a * b));
+            System.out.println("a / b = " + (a / b));
+            System.out.println("a % b = " + (a % b));
+        } else {
+            System.out.println("This program " +
+                    "requires two command-lone arguments");
+        }
+    }
+}
